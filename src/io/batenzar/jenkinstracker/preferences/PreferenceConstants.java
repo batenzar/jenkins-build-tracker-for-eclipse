d124eed462159cf0cac6d5692b0f788cf13e0076
batenzar <batenzar@gmail.com>
2020-08-27 22:17:46 +0700

    initial commit

---
+++ src/io/batenzar/jenkinstracker/preferences/PreferenceConstants.java
@@ -0,0 +1,20 @@
+package io.batenzar.jenkinstracker.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_PATH = "pathPreference";
+
+	public static final String P_BOOLEAN = "booleanPreference";
+
+	public static final String P_CHOICE = "choicePreference";
+
+	public static final String P_STRING = "stringPreference";
+	
+	public static final String URL01 = "url01";
+	public static final String URL02 = "url02";
+	public static final String URL03 = "url03";
+	
+}
